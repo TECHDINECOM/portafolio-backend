4924be2086e10c05dfd716074098f35be8e1f6d9
techdinecom <techdinecom@gmail.com>
2022-11-24 18:48:11 -0300

    Actualice los scripts

--- SCRIPTS_DB/data_basica_de_prueba.sql
+++ SCRIPTS_DB/data_basica_de_prueba.sql
@@ -30,6 +30,11 @@ insert  into `tbl_roles_usuario`(`id_usuario`,`id_rol`) values
 insert  into `tbl_usuario`(`id`,`dni`,`nombres`,`apellidos`,`telefono`,`email`,`sobremi`,`username`,`password`,`estatus`) values 
 (1,95923306,'Ulises José','Hernández Gonzalez','11-21634002','techdinecom@gmail.com','Esto es una prueba','developersys','$2a$12$tf2rnFtHTXrdwkhOGDtJdutAy/M66vaBd1Wrl4z0IVuSjnC1T8abu',1);
 
+
+/*Ordenarla tabla*/
+ALTER TABLE `apyoprogramo`.`tbl_usuario` CHANGE `dni` `dni` BIGINT NOT NULL AFTER `id`, CHANGE `nombres` `nombres` VARCHAR(80) CHARSET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL AFTER `dni`, CHANGE `telefono` `telefono` VARCHAR(11) CHARSET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL AFTER `email`, CHANGE `sobremi` `sobremi` VARCHAR(160) CHARSET utf8mb3 COLLATE utf8mb3_spanish2_ci NULL AFTER `telefono`, CHANGE `username` `username` VARCHAR(16) CHARSET utf8mb3 COLLATE utf8mb3_spanish2_ci NOT NULL AFTER `sobremi`, CHANGE `estatus` `estatus` TINYINT(1) DEFAULT 1 NOT NULL AFTER `password`;
+
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
